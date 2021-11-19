1c825e92cd3348cd1e2425460fd976ea93a43d73
rebelshadowrm <57119466+rebelshadowrm@users.noreply.github.com>
2021-11-19 13:32:18 -0600

    Added custom storefront
    Built out shop items templates and populated front page via API.
    minor mobile responsive tests, seems to scale appropriately.
    
    TODO:
    -redesign css away from bootstrap or attempt to embrace / dodge it while implementing pop out cart.
    -implement local session CRUD for adding items to the cart and updating their quantities.
    --implement UI elements for purchasing items/updating quantities.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -25,38 +25,91 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function handleError(error) {
-        alert('Error, check console');
         console.error(error);
     }
 
     function handleData(data) {
-        // custom product use
+        //shop properties
         console.log(data);
+
+
+        //generate shop
+        const shopRowTemplate = document.querySelector("#shop-row"),
+              shopItemTemplate = document.querySelector("#shop-item"),
+              shopContainer = document.querySelector("#shop-container");
+              
+        
+        
+        
+        
+
+        
+
+        //get list of categories and remove duplicates
+        let arr = [];
+        Object.keys(data).forEach(key => {
+            arr.push(data[key].category);
+        });
+        let unique = Array.from(new Set(arr));
+        unique.forEach(e => {
+            let rowClone = shopRowTemplate.content.cloneNode(true);
+            let category = rowClone.querySelector(".product-type");
+            category.innerText = e;
+            let rowItemContainer = rowClone.querySelector(".row-items");
+
+            data.forEach(elem => {
+                let itemImage = elem.image,
+                itemCategory = elem.category,
+                itemTitle = elem.title,
+                itemPrice = elem.price,
+                itemRating = elem.rating,
+                itemDescription = elem.description,
+                itemId = elem.id;
+                if(itemCategory === e) {
+                    let itemClone = shopItemTemplate.content.cloneNode(true);
+        
+                    let image = itemClone.querySelector(".product-image");
+                    if(image === null) {
+                        image.style.display='none';
+                    } else {
+                        image.src=itemImage;
+                    }
+                    let title = itemClone.querySelector(".product-name");
+                    title.innerText = itemTitle;
+                    let price = itemClone.querySelector(".product-price");
+                    price.innerText = `$${itemPrice}`;
+
+                    rowItemContainer.appendChild(itemClone);
+                }
+            shopContainer.appendChild(rowClone);                   
+            });
+        });
+        
     }
 
-    // table
+    // // table
 
-    var table = new Tabulator("#shop", {
-        ajaxURL: shopAPI,
-        layout: "fitDataFill",
-        autoResize: true,
-        resizableColumns: false,
-        resizableRows: false,
-        responsiveLayout: "collapse",
-        maxHeight: "100%",
-        headerVisible: false, //hide header
-        columns: [
-            {
-                title: "image", field: "image", formatter: "image", function(cell, formatterParams) {
-                    return `<img src="${cell.getValue()}" >`
-                }, width:200, variableHeight: true, responsive:0
-            },
-            { title: "title", field: "title", responsive: 1 },
-            { title: "category", field: "category", responsive: 2 },
-            { title: "description", field: "description", formatter: "textarea", responsive: 0, width:300 },
-            { title: "price", field: "price", formatter: "money", responsive: 3 },
+    // var table = new Tabulator("#shop", {
+    //     ajaxURL: shopAPI,
+    //     layout: "fitDataFill",
+    //     autoResize: true,
+    //     resizableColumns: false,
+    //     resizableRows: false,
+    //     responsiveLayout: "collapse",
+    //     maxHeight: "100%",
+    //     headerVisible: false, //hide header
+    //     columns: [
+    //         {
+    //             title: "image", field: "image", formatter: "image", function(cell, formatterParams) {
+    //                 return `<img src="${cell.getValue()}" >`
+    //             }, width:200, variableHeight: true, responsive:0
+    //         },
+    //         { title: "title", field: "title", responsive: 1 },
+    //         { title: "category", field: "category", responsive: 2 },
+    //         { title: "description", field: "description", formatter: "textarea", responsive: 0, width:300 },
+    //         { title: "price", field: "price", formatter: "money", responsive: 3 },
             
-        ],
-    });
+    //     ],
+    // });
 
 });
