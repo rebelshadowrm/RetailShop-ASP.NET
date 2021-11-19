5a59ebe1fa07399529d544e8daa5cd635069b379
rebelshadowrm <57119466+rebelshadowrm@users.noreply.github.com>
2021-11-19 01:51:00 -0600

    Added FakeStoreAPI call and Tabulator
    Initial setup for API get call.
    Tabulator isn't a good fit for a store page listing of products, however I think that it could be used for the cart.
    
    TODO: will flesh out a template for each item card, then clone templates for each item in the fetch.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,62 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿import Tabulator from 'https://unpkg.com/tabulator-tables@4.9.3/dist/js/tabulator.es2015.min.js';
+const shopAPI = 'https://fakestoreapi.com/products';
 
-// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+
+    //fetch all for future all product usage or fallbacks
+
+    var url = shopAPI,
+        options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    };
+
+    fetch(url, options).then(handleResponse)
+        .then(handleData)
+        .catch(handleError);
+
+    function handleResponse(response) {
+        return response.json().then(function (json) {
+            return response.ok ? json : Promise.reject(json);
+        });
+    }
+
+    function handleError(error) {
+        alert('Error, check console');
+        console.error(error);
+    }
+
+    function handleData(data) {
+        // custom product use
+        console.log(data);
+    }
+
+    // table
+
+    var table = new Tabulator("#shop", {
+        ajaxURL: shopAPI,
+        layout: "fitDataFill",
+        autoResize: true,
+        resizableColumns: false,
+        resizableRows: false,
+        responsiveLayout: "collapse",
+        maxHeight: "100%",
+        headerVisible: false, //hide header
+        columns: [
+            {
+                title: "image", field: "image", formatter: "image", function(cell, formatterParams) {
+                    return `<img src="${cell.getValue()}" >`
+                }, width:200, variableHeight: true, responsive:0
+            },
+            { title: "title", field: "title", responsive: 1 },
+            { title: "category", field: "category", responsive: 2 },
+            { title: "description", field: "description", formatter: "textarea", responsive: 0, width:300 },
+            { title: "price", field: "price", formatter: "money", responsive: 3 },
+            
+        ],
+    });
+
+});
