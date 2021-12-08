7c8e601df4e32b4174df68eda524bb6a08fccd8b
rebelshadowrm <57119466+rebelshadowrm@users.noreply.github.com>
2021-12-07 21:52:16 -0600

    added order history tab in profile

---
+++ Areas/Identity/Pages/Account/Manage/orders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Asp_Group_Project.Areas.Identity.Pages.Account.Manage
+{
+    public class ordersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
